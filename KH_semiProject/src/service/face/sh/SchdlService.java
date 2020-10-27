d66564a42f26a6c387cf69fda7d9d465da829c0a
unknown <4sungnu@naver.com>
2020-10-27 12:13:29 +0900

    C:/Program Files/Git/sh file add

---
+++ KH_semiProject/src/service/face/sh/SchdlService.java
@@ -0,0 +1,18 @@
+package service.face.sh;
+
+import java.util.List;
+
+import dto.Schdl;
+
+public interface SchdlService {
+
+	/**
+	 *  �뒪耳�以� �쟾泥� 議고쉶
+	 *  
+	 * @return 議고쉶�릺 �뒪耳�以� List濡� 諛섑솚
+	 */
+	public List<Schdl> list();
+
+	
+
+}
